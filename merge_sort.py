c3da43b67c8833ff08207703d3816c7e2abda923
HaeunJeong <nicehe74@gmail.com>
2020-06-27 22:00:50 +0900

    merge sort부분 추가

--- merge_sort.py
+++ merge_sort.py
@@ -1,8 +1,8 @@
 def merge(list1, list2):
+    #print(list1, list2)
     result = []
     if len(list1)==0 or len(list2)==0:
         return list1 if len(list1)>0 else list2
-
     while len(list1)>0 or len(list2)>0:
         value1 = list1[0] if len(list1)>0 else 100
         value2 = list2[0] if len(list2)>0 else 100
@@ -11,15 +11,28 @@ def merge(list1, list2):
             del list1[0]
         elif len(list2)>0 and min(value1, value2)==value2:
             del list2[0]
-        
     
     return result
         
     
-# 테스트
-print(merge([1],[]))
+# shift+ option + a : multiline comment
+""" print(merge([1],[]))
 print(merge([],[1]))
 print(merge([2],[1]))
 print(merge([1, 2, 3, 4],[5, 6, 7, 8]))
 print(merge([5, 6, 7, 8],[1, 2, 3, 4]))
-print(merge([4, 7, 8, 9],[1, 3, 6, 10]))
+print(merge([4, 7, 8, 9],[1, 3, 6, 10])) """
+
+
+# 합병 정렬
+def merge_sort(my_list):
+    if len(my_list)<=1:
+        return my_list
+    mid = len(my_list)//2
+    return merge(merge_sort(my_list[:mid]),merge_sort(my_list[mid:]))
+    
+
+# 테스트
+print(merge_sort([1, 3, 5, 7, 9, 11, 13, 11]))
+print(merge_sort([28, 13, 9, 30, 1, 48, 5, 7, 15]))
+print(merge_sort([2, 5, 6, 7, 1, 2, 4, 7, 10, 11, 4, 15, 13, 1, 6, 4]))
