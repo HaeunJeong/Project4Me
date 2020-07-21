cdfee3bb9800defe29530a14d4eb10366eb4806d
HaeunJeong <nicehe74@gmail.com>
2020-07-22 00:18:54 +0900

    Dynamic Programming

---
+++ DP1.py
@@ -0,0 +1,20 @@
+
+def fib_memo(n, cache):
+    if cache.get(n) is not None:
+        return cache.get(n)
+    
+    if n==1 or n==2:
+        cache[n] = 1
+        return cache[n]
+    else:
+        cache[n] = fib_memo(n-1, cache) + fib_memo(n-2, cache)
+        return cache[n]
+    
+def fib(n):
+    fib_cache = {}
+    return fib_memo(n, fib_cache)
+
+# 테스트
+print(fib(10))
+print(fib(50))
+print(fib(100))
